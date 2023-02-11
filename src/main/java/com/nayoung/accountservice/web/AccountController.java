03d49d7da5f5f302861953ecfcc5e5063b6113c6
nayoung <evelyn82ny@gmail.com>
2023-02-11 20:44:22 +0900

    Change "/account-service" to "/"
    - api-gateway-service에 RewritePath Filter 추가

--- src/main/java/com/nayoung/accountservice/web/AccountController.java
+++ src/main/java/com/nayoung/accountservice/web/AccountController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/account-service")
+@RequestMapping("/")
 public class AccountController {
 
     private final AccountService accountService;
