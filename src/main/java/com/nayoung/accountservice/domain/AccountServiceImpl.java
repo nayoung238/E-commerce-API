2f8e871f237991f76b9101317e34fd1494b31a2d
nayoung <evelyn82ny@gmail.com>
2023-02-25 17:21:59 +0900

    refactor: FeignException Handling
    - client 경로를 잘못 설정하면 FeignException 발생 및 Account-service에서의 응답 불가능
    - client 경로를 잘못 설정해도 Account-service에서의 응답할 수 있도록 설정

--- src/main/java/com/nayoung/accountservice/domain/AccountServiceImpl.java
+++ src/main/java/com/nayoung/accountservice/domain/AccountServiceImpl.java
@@ -2,10 +2,15 @@ package com.nayoung.accountservice.domain;
 
 import com.nayoung.accountservice.client.OrderServiceClient;
 import com.nayoung.accountservice.web.dto.AccountResponse;
+import com.nayoung.accountservice.web.dto.OrderResponse;
 import com.nayoung.accountservice.web.dto.SignUpRequest;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -27,7 +32,13 @@ public class AccountServiceImpl implements AccountService {
         Account account = accountRepository.findById(id).orElseThrow();
         AccountResponse response = AccountResponse.fromAccountEntity(account);
 
-        response.setOrders(orderServiceClient.getOrders(id));
+        List<OrderResponse> responseList = new ArrayList<>();
+        try {
+            responseList = orderServiceClient.getOrders(id);
+        } catch (FeignException e) {
+            log.error(e.getMessage());
+        }
+        response.setOrders(responseList);
         return response;
     }
 }
