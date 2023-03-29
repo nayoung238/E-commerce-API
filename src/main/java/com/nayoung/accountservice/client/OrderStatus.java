de166a4931560121a2a4ae41360d41d8942fb8c9
nayoung <evelyn82ny@gmail.com>
2023-03-29 14:13:33 +0900

    Add OrderStatus

---
+++ src/main/java/com/nayoung/accountservice/client/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.nayoung.accountservice.client;
+
+public enum OrderStatus {
+    ACCEPT, SUCCESS, FAILED
+}
--- src/main/java/com/nayoung/accountservice/web/dto/OrderResponse.java
+++ src/main/java/com/nayoung/accountservice/web/dto/OrderResponse.java
@@ -1,11 +1,14 @@
 package com.nayoung.accountservice.web.dto;
 
+import com.nayoung.accountservice.client.OrderStatus;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class OrderResponse {
+    private Long orderId;
+    private OrderStatus orderStatus;
 
     private Long itemId;
     private Long quantity;
