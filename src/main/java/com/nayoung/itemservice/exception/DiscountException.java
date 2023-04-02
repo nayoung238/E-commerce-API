86f852916dfa75410d6473bbe67abaa486243579
nayoung <evelyn82ny@gmail.com>
2023-04-02 17:18:16 +0900

    create custom exceptions (item, discount)

---
+++ src/main/java/com/nayoung/itemservice/exception/DiscountException.java
@@ -0,0 +1,11 @@
+package com.nayoung.itemservice.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class DiscountException extends RuntimeException {
+
+    private final ExceptionCode exceptionCode;
+}
--- src/main/java/com/nayoung/itemservice/exception/ExceptionCode.java
+++ src/main/java/com/nayoung/itemservice/exception/ExceptionCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ExceptionCode {
 
-    INSUFFICIENT_STOCK_EXCEPTION(HttpStatus.BAD_REQUEST, "재고가 부족합니다.");
+    NOT_FOUND_ITEM(HttpStatus.BAD_REQUEST, "존재하지 않는 상품입니다."),
+    INSUFFICIENT_STOCK_EXCEPTION(HttpStatus.BAD_REQUEST, "재고가 부족합니다."),
+    NO_MATCHING_DISCOUNT_CODE(HttpStatus.BAD_REQUEST, "매칭되는 할인이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
