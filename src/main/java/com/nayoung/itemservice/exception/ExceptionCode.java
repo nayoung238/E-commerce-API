8675473121561acf052547c3c7a42c0db9805429
nayoung <evelyn82ny@gmail.com>
2023-04-04 10:14:10 +0900

    create custom exceptions (Shop, Location)

--- src/main/java/com/nayoung/itemservice/exception/ExceptionCode.java
+++ src/main/java/com/nayoung/itemservice/exception/ExceptionCode.java
@@ -10,7 +10,13 @@ public enum ExceptionCode {
 
     NOT_FOUND_ITEM(HttpStatus.BAD_REQUEST, "존재하지 않는 상품입니다."),
     INSUFFICIENT_STOCK_EXCEPTION(HttpStatus.BAD_REQUEST, "재고가 부족합니다."),
-    NO_MATCHING_DISCOUNT_CODE(HttpStatus.BAD_REQUEST, "매칭되는 할인이 없습니다.");
+
+    NO_MATCHING_DISCOUNT_CODE(HttpStatus.BAD_REQUEST, "매칭되는 할인이 없습니다."),
+
+    NOT_FOUND_SHOP(HttpStatus.BAD_REQUEST, "존재하지 않는 상점입니다."),
+    ALREADY_EXIST_SHOP(HttpStatus.BAD_REQUEST, "이미 존재하는 상점입니다."),
+    NON_SERVICE_LOCATION(HttpStatus.BAD_REQUEST, "서비스를 지원하지 않는 지역입니다."),
+    UNAUTHORIZED_MANAGER(HttpStatus.BAD_REQUEST, "권한없는 매니저입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
