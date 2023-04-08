21811bfb312f27acb39ac78d795c5a35c0c9956d
nayoung <evelyn82ny@gmail.com>
2023-04-09 02:34:12 +0900

    feat: multiple items in one order (Kafka)

--- src/main/java/com/nayoung/orderservice/messagequeue/KafkaConsumer.java
+++ src/main/java/com/nayoung/orderservice/messagequeue/KafkaConsumer.java
@@ -3,42 +3,33 @@ package com.nayoung.orderservice.messagequeue;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nayoung.orderservice.domain.Order;
-import com.nayoung.orderservice.domain.OrderRepository;
+import com.nayoung.orderservice.domain.OrderService;
 import com.nayoung.orderservice.domain.OrderStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class KafkaConsumer {
 
-    private final OrderRepository orderRepository;
+    private final OrderService orderService;
 
     @KafkaListener(topics = "update-order-status-topic")
     public void updateOrderStatus(String kafkaMessage) {
-        Map<Object, Object> map = new HashMap<>();
         ObjectMapper mapper = new ObjectMapper();
         try {
-            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {});
+            Map<Object, Object> map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {});
+            Long orderId = Long.parseLong(String.valueOf(map.get("orderId")));
+            boolean isAvailableStockUpdate = Boolean.parseBoolean(String.valueOf(map.get("isAvailableToOrder")));
+            if(isAvailableStockUpdate)
+                orderService.updateOrderStatus(OrderStatus.SUCCEED, orderId);
+            else
+                orderService.updateOrderStatus(OrderStatus.FAILED, orderId);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-
-        Long id = Long.parseLong(String.valueOf(map.get("orderId")));
-        Order order = orderRepository.findById(id).orElseThrow();
-
-        boolean isAvailableStockUpdate = Boolean.parseBoolean(String.valueOf(map.get("available")));
-        if(isAvailableStockUpdate) {
-            order.updateOrderStatus(OrderStatus.SUCCEED);
-        }
-        else {
-            order.updateOrderStatus(OrderStatus.FAILED);
-        }
-        orderRepository.save(order);
     }
 }
