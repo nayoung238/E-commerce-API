a236b525a1b539a4ba4f115b9b9c26210ba37555
nayoung <evelyn82ny@gmail.com>
2023-04-09 12:05:16 +0900

    feat: Multiple items in one order (Kafka Consumer)

--- src/main/java/com/nayoung/itemservice/messagequeue/KafkaConsumer.java
+++ src/main/java/com/nayoung/itemservice/messagequeue/KafkaConsumer.java
@@ -4,15 +4,17 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nayoung.itemservice.domain.item.ItemService;
-import com.nayoung.itemservice.exception.StockException;
-import com.nayoung.itemservice.web.dto.ItemStockUpdateResult;
+import com.nayoung.itemservice.web.dto.ItemStockUpdateRequest;
+import com.nayoung.itemservice.web.dto.ItemStockUpdateResponse;
+import com.nayoung.itemservice.web.dto.OrderItemRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class KafkaConsumer {
@@ -21,24 +23,22 @@ public class KafkaConsumer {
     private final KafkaProducer kafkaProducer;
 
     @KafkaListener(topics = "update-stock-topic")
-    public void updateStock(String kafkaMessage) {
-        Map<Object, Object> map = new HashMap<>();
+    public void updateStock(String kafkaMessage)  {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {});
+            Map<Object, Object> map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {
+            });
+            Object[] orderItems = mapper.convertValue(map.get("orderItems"), Object[].class);
+
+            List<OrderItemRequest> orderItemRequests = new ArrayList<>();
+            for (Object orderItem : orderItems)
+                orderItemRequests.add(OrderItemRequest.fromKafkaMessage(orderItem));
+
+            ItemStockUpdateRequest request = ItemStockUpdateRequest.fromKafkaMessage(map, orderItemRequests);
+            ItemStockUpdateResponse response = itemService.updateItemsStock(request);
+            kafkaProducer.send("update-order-status-topic", response);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-
-        ItemStockUpdateResult itemStockUpdateResult = ItemStockUpdateResult.fromKafkaMessage(map);
-        try {
-            Long itemId = Long.parseLong(String.valueOf(map.get("itemId")));
-            Long quantity = Long.parseLong(String.valueOf(map.get("quantity")));
-            itemService.decreaseStock(itemId, quantity);
-            itemStockUpdateResult.setAvailable(true);
-        } catch (StockException e) {
-            itemStockUpdateResult.setAvailable(false);
-        }
-        kafkaProducer.send("update-order-status-topic", itemStockUpdateResult);
     }
 }
