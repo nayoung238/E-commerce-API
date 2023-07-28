4b461020d91cddb8dcf21d6d4132a88643beec40
nayoung8142 <evelyn82ny@gmail.com>
2023-07-28 15:36:33 +0900

    refactor: service interface 제거

--- src/main/java/com/nayoung/itemservice/domain/discount/DiscountService.java
+++ src/main/java/com/nayoung/itemservice/domain/discount/DiscountService.java
@@ -1,8 +1,24 @@
 package com.nayoung.itemservice.domain.discount;
 
+import com.nayoung.itemservice.domain.item.Item;
+import com.nayoung.itemservice.domain.item.ItemRepository;
+import com.nayoung.itemservice.exception.ExceptionCode;
+import com.nayoung.itemservice.exception.ItemException;
 import com.nayoung.itemservice.web.dto.DiscountCreationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface DiscountService {
+@Service
+@RequiredArgsConstructor
+public class DiscountService {
 
-    void applyDiscount(DiscountCreationRequest request);
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void applyDiscount(DiscountCreationRequest request) {
+        Item item = itemRepository.findByIdWithPessimisticLock(request.getItemId())
+                .orElseThrow(() -> new ItemException(ExceptionCode.NOT_FOUND_ITEM));
+        item.setDiscountPercentage(request.getDiscountPercentage());
+    }
 }
--- src/main/java/com/nayoung/itemservice/domain/shop/ShopService.java
+++ src/main/java/com/nayoung/itemservice/domain/shop/ShopService.java
@@ -1,12 +1,45 @@
 package com.nayoung.itemservice.domain.shop;
 
+import com.nayoung.itemservice.domain.shop.location.CityCode;
+import com.nayoung.itemservice.domain.shop.location.Location;
+import com.nayoung.itemservice.domain.shop.location.ProvinceCode;
+import com.nayoung.itemservice.exception.ExceptionCode;
+import com.nayoung.itemservice.exception.ShopException;
 import com.nayoung.itemservice.web.dto.ShopCreationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface ShopService {
+@Service
+@RequiredArgsConstructor
+public class ShopService {
 
-    void create(ShopCreationRequest request);
-    Shop findShopById(Long shopId);
-    List<Shop> findShops(String province, String city);
+    private final ShopRepository shopRepository;
+
+    public void create(ShopCreationRequest request) {
+        Location location = new Location(request.getProvince(), request.getCity());
+        if(isExistShopName(request.getName())) {
+            throw new ShopException(ExceptionCode.ALREADY_EXIST_SHOP_NAME);
+        }
+        Shop shop = Shop.fromLocationAndName(location, request.getName());
+        shopRepository.save(shop);
+    }
+
+    private boolean isExistShopName(String name) {
+        return shopRepository.findByName(name).isPresent();
+    }
+
+    public Shop findShopById(Long shopId) {
+        return shopRepository.findById(shopId)
+                .orElseThrow(() -> new ShopException(ExceptionCode.NOT_FOUND_SHOP));
+    }
+
+    public List<Shop> findShops(String province, String city) {
+        ProvinceCode provinceCode = ProvinceCode.getProvinceCode(province);
+        CityCode cityCode = CityCode.getCityCode(city);
+
+        if(provinceCode == ProvinceCode.NONE && cityCode == CityCode.NONE) return shopRepository.findAll();
+        return shopRepository.findAllByLocationProvinceAndLocationCity(provinceCode, cityCode);
+    }
 }
