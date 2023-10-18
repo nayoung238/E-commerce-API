904ce6ce651b71a3b8102ad9b6c6f46cf741eacd
nayoung8142 <nayoung8142@gmail.com>
2023-10-18 11:57:34 +0900

    refactor: resilience4j 삭제
    - 주문 생성 방법이 변경되어 resilience4j 사용하지 않음

--- src/main/java/com/nayoung/orderservice/OrderServiceApplication.java
+++ src/main/java/com/nayoung/orderservice/OrderServiceApplication.java
@@ -3,13 +3,11 @@ package com.nayoung.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableJpaAuditing
-@EnableFeignClients
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
