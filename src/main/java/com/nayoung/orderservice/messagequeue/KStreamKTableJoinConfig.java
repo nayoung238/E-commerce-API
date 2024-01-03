f260c342e474d1b5e557d0a11949a6809e167843
nayoung8142 <nayoung8142@gmail.com>
2024-01-03 14:44:12 +0900

    refactor: KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME 설정

--- src/main/java/com/nayoung/orderservice/messagequeue/KStreamKTableJoinConfig.java
+++ src/main/java/com/nayoung/orderservice/messagequeue/KStreamKTableJoinConfig.java
@@ -3,47 +3,42 @@ package com.nayoung.orderservice.messagequeue;
 import com.nayoung.orderservice.domain.OrderItemStatus;
 import com.nayoung.orderservice.web.dto.OrderDto;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
 
 import java.util.HashMap;
-import java.util.Properties;
+import java.util.Map;
 
-@EnableKafka
+@EnableKafkaStreams
 @Configuration
 public class KStreamKTableJoinConfig {
 
-    private final String APPLICATION_ID_CONFIG = "order_application";
     public static final String FINAL_ORDER_CREATION_TOPIC = "e-commerce.order.final-order-details";
-    public static final String ITEM_UPDATE_RESULT_TOPIC = "e-commerce.item.item-update-result";
+    public static final String ORDER_ITEM_UPDATE_RESULT_TOPIC = "e-commerce.item.item-update-result";
 
-    @Bean
-    public Properties kstramProperties() {
-        Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID_CONFIG);
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    public KafkaStreamsConfiguration kafkaStreamsProperties() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "order_application");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, OrderSerde.class);
-        return props;
+        return new KafkaStreamsConfiguration(props);
     }
 
     @Bean
-    public void createOrder() {
-        StreamsBuilder builder = new StreamsBuilder();
-        KTable<String, OrderDto> orderKTable = builder.table(KafkaProducerConfig.TEMPORARY_ORDER_TOPIC);
-        KStream<String, OrderDto> itemUpdateResultStream = builder.stream(ITEM_UPDATE_RESULT_TOPIC);
+    public KStream<String, OrderDto> createOrder(StreamsBuilder streamsBuilder) {
+        KTable<String, OrderDto> requestedOrder = streamsBuilder.table(KafkaProducerConfig.TEMPORARY_ORDER_TOPIC);
+        KStream<String, OrderDto> itemUpdateResult = streamsBuilder.stream(ORDER_ITEM_UPDATE_RESULT_TOPIC);
 
-        itemUpdateResultStream.join(orderKTable, (result, order) -> setOrderStatus(order, result))
-                .to(FINAL_ORDER_CREATION_TOPIC);
-
-        KafkaStreams kafkaStreams = new KafkaStreams(builder.build(), kstramProperties());
-        kafkaStreams.start();
+        return itemUpdateResult.join(requestedOrder, (result, order) -> setOrderStatus(order, result));
     }
 
     private OrderDto setOrderStatus(OrderDto orderDto, OrderDto result) {
