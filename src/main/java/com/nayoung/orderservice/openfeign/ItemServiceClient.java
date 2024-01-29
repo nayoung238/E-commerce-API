e074d3fbf4e203bd9a42313b47e6431482b74f70
nayoung8142 <nayoung8142@gmail.com>
2024-01-30 02:31:09 +0900

    refactor: OpenFeign 설정

--- src/main/java/com/nayoung/orderservice/openfeign/ItemServiceClient.java
+++ src/main/java/com/nayoung/orderservice/openfeign/ItemServiceClient.java
@@ -1,19 +1,14 @@
 package com.nayoung.orderservice.openfeign;
 
-import com.nayoung.orderservice.openfeign.dto.ItemUpdateLogDto;
-import io.github.resilience4j.retry.annotation.Retry;
+import com.nayoung.orderservice.domain.OrderItemStatus;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-
-@FeignClient(name = "item-service",
-        url = "http://127.0.0.1:8080/item-service/%s")
+@FeignClient(name = "item-service", url = "http://127.0.0.1:8089/item-service")
 public interface ItemServiceClient {
 
-    @Retry(name = "order-result")
-    @GetMapping(value = "/item-update-log/{eventId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    List<ItemUpdateLogDto> findAllOrderItemUpdateResultByEventId(@PathVariable String eventId);
+    @GetMapping(value = "/order-processing-result/{eventId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    OrderItemStatus findOrderProcessingResultByEventId(@PathVariable String eventId);
 }
--- src/main/java/com/nayoung/orderservice/openfeign/OpenFeignConfig.java
+++
@@ -1,9 +0,0 @@
-package com.nayoung.orderservice.openfeign;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFeignClients(basePackages = "com.nayoung.orderservice.openfeign")
-public class OpenFeignConfig {
-}
