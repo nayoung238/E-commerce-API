19026a23d1964aea099557e2e5be5abbda8f1002
nayoung8142 <nayoung8142@gmail.com>
2024-02-14 02:13:23 +0900

    refactor: @Builder 설정

--- src/main/java/com/nayoung/accountservice/domain/Account.java
+++ src/main/java/com/nayoung/accountservice/domain/Account.java
@@ -1,17 +1,11 @@
 package com.nayoung.accountservice.domain;
 
 import com.nayoung.accountservice.web.dto.SignUpDto;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity
+@Entity @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -23,15 +17,15 @@ public class Account {
 
     private String email;
     private String password;
+
+    @Column(unique = true)
     private String name;
 
-    private Account (SignUpDto signUpRequest) {
-        this.email = signUpRequest.getEmail();
-        this.password = signUpRequest.getPassword();
-        this.name = signUpRequest.getName();
-    }
-
     public static Account fromAccountDto(SignUpDto signUpRequest) {
-        return new Account(signUpRequest);
+        return Account.builder()
+                .email(signUpRequest.getEmail())
+                .password(signUpRequest.getPassword())
+                .name(signUpRequest.getName())
+                .build();
     }
 }
