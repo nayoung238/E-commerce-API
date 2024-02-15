a4acb44255f456aa3507e04f0b976e1fb9c95cd8
nayoung8142 <nayoung8142@gmail.com>
2024-02-15 09:28:48 +0900

    refactor: CircuitBreaker, Retry configuration 분리

---
+++ src/main/java/com/ecommerce/accountservice/resilience4j/Resilience4jRetryConfig.java
@@ -0,0 +1,27 @@
+package com.ecommerce.accountservice.resilience4j;
+
+import feign.FeignException;
+import io.github.resilience4j.core.IntervalFunction;
+import io.github.resilience4j.retry.RetryConfig;
+import io.github.resilience4j.retry.RetryRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class Resilience4jRetryConfig {
+
+    public static final String ORDER_LIST_RETRY = "orderListRetry";
+
+    @Bean(name = ORDER_LIST_RETRY)
+    public RetryRegistry retryRegistry() {
+        return RetryRegistry.of(
+                RetryConfig.custom()
+                        .maxAttempts(3)
+                        .intervalFunction(IntervalFunction.ofExponentialRandomBackoff(Duration.ofMillis(5000), 2))
+                        .retryExceptions(FeignException.FeignClientException.class)
+                        .retryOnException(throwable -> !(throwable instanceof FeignException.FeignServerException))
+                        .build());
+    }
+}
