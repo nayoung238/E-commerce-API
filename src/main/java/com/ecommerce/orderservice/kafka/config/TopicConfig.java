502542ba489cada75db918523142e307d91799e8
nayoung8142 <nayoung8142@gmail.com>
2024-03-08 00:42:21 +0900

    refactor: @Configuration 설정

--- src/main/java/com/ecommerce/orderservice/kafka/config/TopicConfig.java
+++ src/main/java/com/ecommerce/orderservice/kafka/config/TopicConfig.java
@@ -1,9 +1,11 @@
 package com.ecommerce.orderservice.kafka.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaAdmin;
 
+@Configuration
 public class TopicConfig {
 
     public static final String REQUESTED_ORDER_TOPIC = "e-commerce.order.requested-order-details";
@@ -14,9 +16,9 @@ public class TopicConfig {
     public static final String ORDER_PROCESSING_RESULT_STREAMS_ONLY_TOPIC = "e-commerce.item.item-update-result-streams-only";
     public static final String FINAL_ORDER_STREAMS_ONLY_TOPIC = "e-commerce.order.final-order-details-streams-only";
 
-//    @Bean
-//    public KafkaAdmin.NewTopics newTopics() {
-//        return new KafkaAdmin.NewTopics(
+    @Bean
+    public KafkaAdmin.NewTopics newTopics() {
+        return new KafkaAdmin.NewTopics(
 //                TopicBuilder.name(REQUESTED_ORDER_TOPIC)
 //                        .partitions(1)
 //                        .replicas(1)
@@ -26,16 +28,16 @@ public class TopicConfig {
 //                        .partitions(1)
 //                        .replicas(1)
 //                        .build(),
-//
-//                TopicBuilder.name(ORDER_PROCESSING_RESULT_REQUEST_STREAMS_ONLY_TOPIC)
-//                        .partitions(1)
-//                        .replicas(1)
-//                        .build(),
-//
-//                TopicBuilder.name(FINAL_ORDER_STREAMS_ONLY_TOPIC)
-//                        .partitions(1)
-//                        .replicas(1)
-//                        .build()
-//        );
-//    }
+
+                TopicBuilder.name(ORDER_PROCESSING_RESULT_REQUEST_STREAMS_ONLY_TOPIC)
+                        .partitions(1)
+                        .replicas(1)
+                        .build(),
+
+                TopicBuilder.name(FINAL_ORDER_STREAMS_ONLY_TOPIC)
+                        .partitions(1)
+                        .replicas(1)
+                        .build()
+        );
+    }
 }
