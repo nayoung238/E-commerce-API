97c50fb55f23987d2b9a3049e9c6ec6706cbb150
nayoung8142 <nayoung8142@gmail.com>
2024-03-07 19:42:27 +0900

    refactor: AdminClient 적용

---
+++ src/main/java/com/ecommerce/itemservice/kafka/service/streams/StockAggregationService.java
@@ -0,0 +1,62 @@
+package com.ecommerce.itemservice.kafka.service.streams;
+
+import com.ecommerce.itemservice.domain.item.service.ItemStockService;
+import com.ecommerce.itemservice.kafka.config.TopicConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.WindowStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class StockAggregationService {
+
+    private final ItemStockService itemStockService;
+
+    @Bean
+//    @KafkaListener(topics = TopicConfig.ITEM_UPDATE_LOG_TOPIC)
+    public KStream<Windowed<String>, Long> calculateTotalQuantity(KafkaStreamsConfiguration kafkaStreamsConfiguration,
+                                                                  StreamsBuilder streamsBuilder) {
+        AdminClient adminClient = AdminClient.create(kafkaStreamsConfiguration.asProperties());
+        adminClient.createTopics(Collections.singleton(
+                new NewTopic(TopicConfig.ITEM_UPDATE_LOG_TOPIC, 1, (short) 1)));
+
+        KStream<String, Long> stream = streamsBuilder.stream(TopicConfig.ITEM_UPDATE_LOG_TOPIC);
+
+        return stream
+                .groupByKey()
+                .windowedBy(TimeWindows.ofSizeAndGrace(Duration.ofSeconds(5), Duration.ofSeconds(5)))
+                .reduce(Long::sum,
+                        Materialized
+                                .<String, Long, WindowStore<Bytes, byte[]>>as("total-quantity")
+                                .withKeySerde(Serdes.String())
+                                .withValueSerde(Serdes.Long())
+                                .withRetention(Duration.ofMinutes(1))
+                        //.withLoggingEnabled(Collections.singletonMap("min.insync.replicas", "1"))
+                )
+                .suppress(Suppressed.untilWindowCloses(Suppressed.BufferConfig.unbounded().shutDownWhenFull()))
+                .toStream()
+                .peek((key, value) -> {
+                    Long itemId = Long.valueOf(key.key());
+                    itemStockService.updateStockWithPessimisticLock(itemId, value);
+                    log.info("Aggregation results [ {} - {} ] -> itemId={}, quantity={}",
+                            key.window().startTime(),
+                            key.window().endTime(),
+                            itemId,
+                            value);
+                });
+    }
+}
