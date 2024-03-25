621c34b2ee8187142fbfdf4c57bc2068950d1494
nayoung8142 <nayoung8142@gmail.com>
2024-03-26 00:40:16 +0900

    패키지명 변경

--- src/main/java/com/ecommerce/apigatewayservice/ApiGatewayServiceApplication.java
+++ src/main/java/com/ecommerce/apigatewayservice/ApiGatewayServiceApplication.java
@@ -1,4 +1,4 @@
-package com.nayoung.apigatewayservice;
+package com.ecommerce.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ecommerce/apigatewayservice/ApiGatewayServiceApplicationTests.java
+++ src/test/java/com/ecommerce/apigatewayservice/ApiGatewayServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nayoung.apigatewayservice;
+package com.ecommerce.apigatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
