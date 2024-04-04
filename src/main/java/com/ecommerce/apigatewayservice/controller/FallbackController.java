8c14e1c087b9b735415a1ac22d4ec4addfc196a7
nayoung8142 <nayoung8142@gmail.com>
2024-04-04 19:40:53 +0900

    Circuit Breaker 설정

---
+++ src/main/java/com/ecommerce/apigatewayservice/controller/FallbackController.java
@@ -0,0 +1,33 @@
+package com.ecommerce.apigatewayservice.controller;
+
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.support.NotFoundException;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.Objects;
+import java.util.concurrent.TimeoutException;
+
+@RestController
+@Slf4j
+public class FallbackController {
+
+    @GetMapping("/fallback")
+    public void serviceFallback(ServerWebExchange exchange) {
+        Throwable t = exchange.getAttribute(ServerWebExchangeUtils.CIRCUITBREAKER_EXECUTION_EXCEPTION_ATTR);
+
+        // TODO: 수정 예정
+        if(t instanceof NotFoundException) {
+            log.error(t.toString());
+        } else if(t instanceof TimeoutException) {
+            log.error(t.toString());
+        } else if(t instanceof CallNotPermittedException) {
+            log.error(t.toString());
+        } else {
+            log.error(Objects.requireNonNull(t).toString());
+        }
+    }
+}
