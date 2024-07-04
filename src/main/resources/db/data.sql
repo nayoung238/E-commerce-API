3b1353f3f07f3f2750e9abc9011c6758e3bc84ed
imzero238 <nayoung8142@gmail.com>
2024-07-04 20:15:01 +0900

    초기 데이터 생성

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,4 @@
+insert into Item (stock, version, name) values (100, 0, 'test-item-1'),
+                                               (100, 0, 'test-item-2'),
+                                               (10, 0, 'test-item-3'),
+                                               (10, 0, 'test-item-4');
