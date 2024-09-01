1648838e67c82b1fc0d2839c0ace75e63fd47d4a
imzero238 <nayoung8142@gmail.com>
2024-09-01 19:46:19 +0900

    Feat: Dummy Data 전송 서비스
    - Kafka Streams Aggregation에서 사용

--- src/main/java/com/ecommerce/itemservice/ItemServiceApplication.java
+++ src/main/java/com/ecommerce/itemservice/ItemServiceApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ItemServiceApplication {
 
     public static void main(String[] args) {
