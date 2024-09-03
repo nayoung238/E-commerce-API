30c8b2367cc6bccf2921d3207a6b9f4826df7983
imzero238 <nayoung8142@gmail.com>
2024-09-03 09:33:24 +0900

    Test: @ActiveProfiles 제거

--- src/test/java/com/ecommerce/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/com/ecommerce/orderservice/OrderServiceApplicationTests.java
@@ -2,10 +2,8 @@ package com.ecommerce.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("local")
 class OrderServiceApplicationTests {
 
     @Test
