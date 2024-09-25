dd1739ad031d3157c99f1dc97e80adec05a21dd9
imzero238 <nayoung8142@gmail.com>
2024-09-25 09:36:55 +0900

    Refactor: 서비스명, 컨트롤러명 변경

--- src/main/java/com/ecommerce/couponservice/api/CouponManagementController.java
+++ src/main/java/com/ecommerce/couponservice/api/CouponManagementController.java
@@ -2,7 +2,7 @@ package com.ecommerce.couponservice.api;
 
 import com.ecommerce.couponservice.domain.coupon.dto.CouponDto;
 import com.ecommerce.couponservice.domain.coupon.dto.CouponRegisterRequestDto;
-import com.ecommerce.couponservice.domain.coupon.service.CouponService;
+import com.ecommerce.couponservice.domain.coupon.service.CouponManagementService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
@@ -12,21 +12,21 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/coupons")
+@RequestMapping("/admin/coupons")
 @Validated
-public class CouponController {
+public class CouponManagementController {
 
-    private final CouponService couponService;
+    private final CouponManagementService couponManagementService;
 
     @PostMapping
     public ResponseEntity<?> register(@RequestBody @Valid CouponRegisterRequestDto couponRegisterRequestDto) {
-        CouponDto response = couponService.register(couponRegisterRequestDto);
+        CouponDto response = couponManagementService.register(couponRegisterRequestDto);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<CouponDto> findCouponById(@PathVariable @Valid @Positive(message = "쿠폰 아이디는 1 이상이어야 합니다.") Long id) {
-        CouponDto response = couponService.findCouponById(id);
+        CouponDto response = couponManagementService.findCouponById(id);
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/ecommerce/couponservice/domain/coupon/service/CouponManagementService.java
+++ src/main/java/com/ecommerce/couponservice/domain/coupon/service/CouponManagementService.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class CouponService {
+public class CouponManagementService {
 
     private final CouponRepository couponRepository;
 
