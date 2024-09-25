ecb34a0d905347e4800adffcf3dd25b2f48aabb5
imzero238 <nayoung8142@gmail.com>
2024-09-25 11:05:17 +0900

    refactor: health-check API 추가 및 path 변경

--- src/main/java/com/ecommerce/accountservice/api/AccountController.java
+++ src/main/java/com/ecommerce/accountservice/api/AccountController.java
@@ -1,16 +1,19 @@
 package com.ecommerce.accountservice.api;
 
 import com.ecommerce.accountservice.api.dto.DetailedAccountDto;
-import com.ecommerce.accountservice.domain.AccountService;
+import com.ecommerce.accountservice.domain.service.AccountService;
 import com.ecommerce.accountservice.api.dto.SimpleAccountDto;
 import com.ecommerce.accountservice.api.dto.SignUpRequestDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/accounts")
+@Slf4j
 public class AccountController {
 
     private final AccountService accountService;
@@ -21,15 +24,21 @@ public class AccountController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @GetMapping("/accounts/simple/{accountId}")
+    @GetMapping("/simple/{accountId}")
     public ResponseEntity<?> findSimpleAccountInfo(@PathVariable Long accountId) {
         SimpleAccountDto response = accountService.findSimpleAccountInfoById(accountId);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    @GetMapping("/accounts/details/{accountId}")
+    @GetMapping("/details/{accountId}")
     public ResponseEntity<?> findDetailedAccountInfo(@PathVariable Long accountId) {
         DetailedAccountDto response = accountService.findDetailedAccountInfoById(accountId);
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/health-check")
+    public ResponseEntity<?> healthCheck() {
+        log.info("health check...");
+        return ResponseEntity.ok("health check...");
     }
 }
