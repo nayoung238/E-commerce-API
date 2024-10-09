3e6d9be467307f57799f5f346f85186aaa8c7cbc
imzero238 <nayoung8142@gmail.com>
2024-10-09 22:50:30 +0900

    Refactor: Coupon registration to use Redis transaction (SessionCallback)

---
+++ src/main/java/com/ecommerce/couponservice/redis/manager/CouponHashName.java
@@ -0,0 +1,6 @@
+package com.ecommerce.couponservice.redis.manager;
+
+public enum CouponHashName {
+    STOCK,
+    NAME
+}
--- src/main/java/com/ecommerce/couponservice/redis/manager/CouponStockRedisManager.java
+++ src/main/java/com/ecommerce/couponservice/redis/manager/CouponStockRedisManager.java
@@ -2,7 +2,9 @@ package com.ecommerce.couponservice.redis.manager;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SessionCallback;
@@ -19,31 +21,49 @@ public class CouponStockRedisManager extends BaseRedisManager {
 
     private final RedisTemplate<String, String> redisTemplate;
 
-    public void registerCouponStock(Long couponId, Long quantity) {
-        redisTemplate.opsForHash()
-                .put(COUPON_STOCK_KEY, couponId.toString(), quantity.toString());
+    public void registerCoupon(Long couponId, String name, Long quantity) {
+        String couponKey = getCouponKey(couponId);
+        redisTemplate.execute(new SessionCallback<List<Object>>() {
+            @Override
+            public List<Object> execute(@NotNull RedisOperations operations) throws DataAccessException {
+                operations.multi();
+
+                @SuppressWarnings("unchecked")
+                HashOperations<String, String, String> hashOps = operations.opsForHash();
+                hashOps.put(couponKey, CouponHashName.NAME.name(), name);
+                hashOps.put(couponKey, CouponHashName.STOCK.name(), quantity.toString());
+
+                return operations.exec();
+            }
+        });
     }
 
     public Optional<Long> getStock(Long couponId) {
-        Object value = redisTemplate.opsForHash().get(COUPON_STOCK_KEY, couponId.toString());
+        Object value = redisTemplate.opsForHash().get(getCouponKey(couponId), CouponHashName.STOCK.name());
         return Optional.ofNullable(value)
                 .map(v -> Long.parseLong(v.toString()));
     }
 
+    public String getCouponName(Long couponId) {
+        Object value = redisTemplate.opsForHash().get(getCouponKey(couponId), CouponHashName.NAME.name());
+        return value != null ? value.toString() : null;
+    }
+
     public CouponIssuanceStatus decrementStock(Long couponId, Long accountId) {
         return redisTemplate.execute(new SessionCallback<CouponIssuanceStatus>() {
-
             @Override
             public <K, V> CouponIssuanceStatus execute(RedisOperations<K, V> operations) throws DataAccessException {
                 operations.multi();
+
+                @SuppressWarnings("unchecked")
                 RedisOperations<String, String> pipelinedOps = (RedisOperations<String, String>) operations;
 
+                String couponKey = getCouponKey(couponId);
                 String waitQueueKey = getWaitQueueKey(couponId);
                 String enterQueueKey = getEnterQueueKey(couponId);
 
                 pipelinedOps.opsForZSet().score(waitQueueKey, accountId.toString());
-                pipelinedOps.opsForHash().get(COUPON_STOCK_KEY, couponId.toString());
-                pipelinedOps.opsForHash().increment(COUPON_STOCK_KEY, couponId.toString(), -1);
+                pipelinedOps.opsForHash().increment(couponKey, CouponHashName.STOCK.name(), -1);
                 pipelinedOps.opsForZSet().remove(enterQueueKey, accountId.toString());
 
                 List<Object> results = operations.exec();
@@ -52,21 +72,13 @@ public class CouponStockRedisManager extends BaseRedisManager {
                 }
 
                 Double wasInWaitQueue = (Double) results.get(0);
-                String currentStockStr = (String) results.get(1);
-                Long newStock = (Long) results.get(2);
-                Long removedFromEnterQueue = (Long) results.get(3);
+                Long newStock = (Long) results.get(1);
+                Long removedFromEnterQueue = (Long) results.get(2);
 
                 if (wasInWaitQueue != null) {
                     return CouponIssuanceStatus.ALREADY_IN_WAIT_QUEUE;
                 }
-                if (currentStockStr == null) {
-                    return CouponIssuanceStatus.NOT_FOUND_COUPON;
-                }
-                Long currentStock = parseLongSafely(currentStockStr);
-                if (currentStock == null) {
-                    return CouponIssuanceStatus.UNKNOWN_ERROR;
-                }
-                if (currentStock <= 0 || newStock < 0) {
+                if (newStock < 0) {
                     return CouponIssuanceStatus.OUT_OF_STOCK;
                 }
                 if (removedFromEnterQueue == 1) {
@@ -84,7 +96,7 @@ public class CouponStockRedisManager extends BaseRedisManager {
                 RedisOperations<String, String> pipelinedOps = (RedisOperations<String, String>) operations;
                 pipelinedOps.multi();
 
-                pipelinedOps.opsForHash().increment(COUPON_STOCK_KEY, couponId.toString(), 1);
+                pipelinedOps.opsForHash().increment(getCouponKey(couponId), CouponHashName.STOCK.name(), 1);
 
                 pipelinedOps.exec();
                 return null;
@@ -92,15 +104,7 @@ public class CouponStockRedisManager extends BaseRedisManager {
         });
     }
 
-    private Long parseLongSafely(String value) {
-        try {
-            return Long.parseLong(value);
-        } catch (NumberFormatException e) {
-            return null;
-        }
-    }
-
     public Set<String> getCouponStockHashKeys() {
-        return redisTemplate.keys(COUPON_STOCK_KEY + "*");
+        return redisTemplate.keys(COUPON_KEY + "*");
     }
 }
