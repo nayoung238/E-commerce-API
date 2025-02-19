d291a92a401b49b6b6a9487210259b657713ce32
imzero238 <nayoung8142@gmail.com>
2025-02-19 23:14:46 +0900

    refactor: coupon 도메인 제거
    - SRP 위반

--- account-api/src/main/java/com/ecommerce/accountservice/account/entity/Account.java
+++ account-api/src/main/java/com/ecommerce/accountservice/account/entity/Account.java
@@ -4,8 +4,6 @@ import com.ecommerce.accountservice.account.dto.SignUpRequestDto;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.Map;
-
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -24,17 +22,11 @@ public class Account {
     @Column(nullable = false, unique = true)
     private String name;
 
-    @ElementCollection(fetch = FetchType.LAZY)
-    @CollectionTable(name = "user_coupon", joinColumns = @JoinColumn(name = "account_id"))
-    @Column(name = "coupon_name")
-    private Map<Long, String> coupons;
-
     @Builder(access = AccessLevel.PRIVATE)
-    private Account(String loginId, String password, String name, Map<Long, String> coupons) {
+    private Account(String loginId, String password, String name) {
         this.loginId = loginId;
         this.password = password;
         this.name = name;
-        this.coupons = coupons;
     }
 
     public static Account of(SignUpRequestDto signUpRequest) {
@@ -43,9 +35,5 @@ public class Account {
                 .password(signUpRequest.password())
                 .name(signUpRequest.name())
                 .build();
-    }
-
-    public void addCoupon(Long couponId, String couponName) {
-        this.coupons.put(couponId, couponName);
     }
 }
