046dfa5edfe8fc5dd62c0d2cd9058090aade2dfc
imzero238 <nayoung8142@gmail.com>
2025-02-19 23:44:14 +0900

    feat: CustomException 생성

--- account-api/src/main/java/com/ecommerce/accountservice/exception/GlobalExceptionHandler.java
+++ account-api/src/main/java/com/ecommerce/accountservice/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -21,5 +22,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
             .body(Objects.requireNonNull(ex.getBindingResult().getFieldError()).getDefaultMessage());
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<String> customException(CustomException ex){
+        return ResponseEntity
+            .status(ex.getErrorCode().getHttpStatus())
+            .body(ex.getErrorCode().getMessage());
     }
 }
