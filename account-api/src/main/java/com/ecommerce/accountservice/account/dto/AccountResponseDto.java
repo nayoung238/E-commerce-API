c3b22337b1ee34af5e42633e1192b63eedcf0c2f
imzero238 <nayoung8142@gmail.com>
2025-02-20 00:05:32 +0900

    chore: AccountResponseDto에 accountId 추가

--- account-api/src/main/java/com/ecommerce/accountservice/account/dto/AccountResponseDto.java
+++ account-api/src/main/java/com/ecommerce/accountservice/account/dto/AccountResponseDto.java
@@ -6,12 +6,14 @@ import lombok.Builder;
 @Builder
 public record AccountResponseDto (
 
+    long accountId,
     String loginId,
     String name
 ) {
 
     public static AccountResponseDto of(Account account) {
         return AccountResponseDto.builder()
+                .accountId(account.getId())
                 .loginId(account.getLoginId())
                 .name(account.getName())
                 .build();
