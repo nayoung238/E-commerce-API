b79379121fce2c1357b9eb356779fb58e0a3053b
imzero238 <nayoung8142@gmail.com>
2025-02-24 17:01:51 +0900

    Add api-composer module

---
+++ api-composer/src/main/java/com/ecommerce/apicomposer/ApiComposerApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.apicomposer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComposerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiComposerApplication.class, args);
+	}
+
+}
---
+++ api-composer/src/test/java/com/ecommerce/apicomposer/ApiComposerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.apicomposer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiComposerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
