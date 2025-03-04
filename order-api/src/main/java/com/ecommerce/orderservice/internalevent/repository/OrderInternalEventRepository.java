a3cf894c02bca18e7437e80f0dbc6637c63c3d5b
imzero238 <nayoung8142@gmail.com>
2025-03-04 17:37:40 +0900

    fix: 패키지 설정

--- order-api/src/main/java/com/ecommerce/orderservice/internalevent/repository/OrderInternalEventRepository.java
+++ order-api/src/main/java/com/ecommerce/orderservice/internalevent/repository/OrderInternalEventRepository.java
@@ -1,6 +1,6 @@
-package com.ecommerce.orderservice.internalevent.order.repository;
+package com.ecommerce.orderservice.internalevent.repository;
 
-import com.ecommerce.orderservice.internalevent.order.event.OrderInternalEvent;
+import com.ecommerce.orderservice.internalevent.entity.OrderInternalEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
