0668efc1f6688aa57205feb6b58c9f091118585b
imzero238 <nayoung8142@gmail.com>
2025-03-14 01:27:19 +0900

    test: @DisplayName 수정

--- item-api/src/test/java/com/ecommerce/itemservice/item/service/ItemStockServiceUnitTest.java
+++ item-api/src/test/java/com/ecommerce/itemservice/item/service/ItemStockServiceUnitTest.java
@@ -33,61 +33,52 @@ class ItemStockServiceUnitTest extends UnitTestSupport {
     @Mock
     StockUpdateService stockUpdateService;
 
-    @DisplayName("최초 이벤트는 주문에 속한 모든 아이템에 대한 재고 변경 작업과 결과 이벤트 발행")
+    @DisplayName("[Initial Event 테스트] 최초 이벤트는 주문에 속한 모든 아이템에 대한 재고 변경 작업과 결과 이벤트 발행")
     @Test
-    void 최초_이벤트_행동_검증 () {
-        // setup (data)
+    void initial_event_test () {
+        // given
         final List<Long> itemIds = List.of(1L, 3L);
         OrderKafkaEvent event = getOrderKafkaEvent(2L, itemIds, 12L, OrderProcessingStatus.PROCESSING);
 
-        // setup(expectations)
-        Mockito.when(orderRedisRepository.addEventId(anyString(), anyString()))
-                .thenReturn(1L); // Redis 캐싱 성공
-
+        Mockito.when(orderRedisRepository.addEventId(anyString(), anyString())).thenReturn(1L);
         Mockito.when(stockUpdateService.updateStock(any(), any()))
                 .thenReturn(getOrderItemKafkaEvent(1L, 1L, OrderProcessingStatus.SUCCESSFUL));
 
-        // exercise
+        // when
         itemStockService.updateStock(event, false);
 
         // verify
-        // Redis에서 이벤트 처리 여부 확인 및 이벤트 캐싱
-        verify(orderRedisRepository, times(1))
-                .addEventId(anyString(), anyString());
+        // 레디스에 이벤트 처리 여부 확인 및 이벤트 캐싱
+        verify(orderRedisRepository, times(1)).addEventId(anyString(), anyString());
 
         // 주문에 속한 모든 아이템에 대한 재고 변경 작업
-        verify(stockUpdateService, times(itemIds.size()))
-                .updateStock(any(OrderItemKafkaEvent.class), any());
+        verify(stockUpdateService, times(itemIds.size())).updateStock(any(OrderItemKafkaEvent.class), any());
 
-        // Redis에 이벤트 처리 결과 캐싱
-        verify(orderRedisRepository, times(1))
-                .setOrderProcessingStatus(anyString(), any());
+        // 레디스에 이벤트 처리 결과 캐싱
+        verify(orderRedisRepository, times(1)).setOrderProcessingStatus(anyString(), any());
 
         // 요청에 대한 결과 이벤트 발행
-        verify(kafkaProducerService,times(1))
-                .sendMessage(anyString(), anyString(), any(OrderKafkaEvent.class));
+        verify(kafkaProducerService,times(1)).sendMessage(anyString(), anyString(), any(OrderKafkaEvent.class));
     }
 
-    @DisplayName("재요청 이벤트는 결과만 반환")
+    @DisplayName("[Retry Event 테스트] 재요청 이벤트는 결과만 반환")
     @Test
-    void 재요청_행동_검증 () {
-        // setup (data)
+    void retry_event_test () {
+        // given
         final List<Long> itemIds = List.of(1L, 3L);
         OrderKafkaEvent event = getOrderKafkaEvent(2L, itemIds, 12L, OrderProcessingStatus.PROCESSING);
 
-        // setup(expectations)
         Mockito.when(orderRedisRepository.addEventId(anyString(), anyString()))
                 .thenReturn(0L); // Redis 이미 캐싱됨
 
-        // exercise
+        // when
         itemStockService.updateStock(event, false);
 
+        // then
         // 주문에 속한 모든 아이템에 대한 재고 변경 작업
-        verify(stockUpdateService, times(0))
-                .updateStock(any(OrderItemKafkaEvent.class), any());
+        verify(stockUpdateService, times(0)).updateStock(any(OrderItemKafkaEvent.class), any());
 
-        // Redis에서 이벤트 처리 결과 조회
-        verify(orderRedisRepository, times(1))
-                .getOrderProcessingStatus(anyString());
+        // 레디스에서 이벤트 처리 결과 조회
+        verify(orderRedisRepository, times(1)).getOrderProcessingStatus(anyString());
     }
 }
